eddcef246aa60ba2bf9d2d24ea345a59abbe79ca
Alfred <kalumunabisimwa5@gmail.com>
2022-03-22 16:04:55 +0200

    use style with webpack

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -6,4 +6,16 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
 };
